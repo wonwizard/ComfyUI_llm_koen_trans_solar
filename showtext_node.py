eae17afc66dc8b5e07da2e6d9ddba9a8f414d312
wonwizard <54572615+wonwizard@users.noreply.github.com>
2024-03-04 22:18:46 +0900

    Update showtext_node.py

--- showtext_node.py
+++ showtext_node.py
@@ -9,18 +9,26 @@ class ShowText:
                     "default": ""
                 }),    
             },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
         }
 
     INPUT_IS_LIST = True
     RETURN_TYPES = ("STRING",)
-    
     FUNCTION = "run"
     OUTPUT_NODE = True
     OUTPUT_IS_LIST = (True,)
+    
     CATEGORY = "LLM Translate"
     
-    def run(self, text, string_field):
-        
+    def run(self, text, string_field, unique_id = None, extra_pnginfo=None):
+        if unique_id and extra_pnginfo and "workflow" in extra_pnginfo[0]:
+            workflow = extra_pnginfo[0]["workflow"]
+            node = next((x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]), None)
+            if node:
+                node["widgets_values"] = [text]        
         return {"ui": {"text": text}, "result": (text,)}
 
 
