3cab92f56b3b9f97ef70e175d6eb6d74a47831a1
wonwizard <54572615+wonwizard@users.noreply.github.com>
2024-03-04 21:47:22 +0900

    Update showtext_node.py

--- showtext_node.py
+++ showtext_node.py
@@ -3,19 +3,21 @@ class ShowText:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "text": ("STRING", {"multiline": True, "forceInput": True}),
+                "text": ("STRING", {"forceInput": True}),
+                "string_field": ("STRING", {
+                    "multiline": True, #True if you want the field to look like the one on the ClipTextEncode node
+                    "default": ""
+                }),    
             },
         }
 
-    INPUT_IS_LIST = True
     RETURN_TYPES = ("STRING",)
-    OUTPUT_NODE = True
-    OUTPUT_IS_LIST = (True,)
-
+    
     FUNCTION = "run"
     CATEGORY = "LLM Translate"
 
-    def run(self, text):
+    def run(self, text, string_field):
+        
         return {"ui": {"text": text}, "result": (text,)}
 
 
