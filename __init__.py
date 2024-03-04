e79df30568c88942f6a13e58ab35470be5dc58e4
wonwizard <54572615+wonwizard@users.noreply.github.com>
2024-03-04 20:51:55 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
-from .node_solar_koen import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .node_solar_koen import *
+from .showtext_node import *
+
+NODE_CLASS_MAPPINGS = {
+    "LLMTrans": LLMTrans,
+    "ShowText": ShowText,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LLMTrans": "LLM Translate(ko<->En) node",
+    "ShowText": "Show Text",
+}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
