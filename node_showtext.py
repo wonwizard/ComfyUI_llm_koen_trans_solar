4579f4048829e5423e8ae113d24adc1c78f5e6ce
wonwizard <54572615+wonwizard@users.noreply.github.com>
2024-03-05 22:27:55 +0900

    Update node_showtext.py

--- node_showtext.py
+++ node_showtext.py
@@ -13,20 +13,19 @@ class ShowText:
 
     INPUT_IS_LIST = True
     RETURN_TYPES = ("STRING",)
-    FUNCTION = "run"
+    FUNCTION = "notify"
     OUTPUT_NODE = True
     OUTPUT_IS_LIST = (True,)
-    
-    CATEGORY = "LLM Translate"
-    
-    def run(self, text, unique_id = None, extra_pnginfo=None):
+
+    CATEGORY = "utils"
+
+    def notify(self, text, unique_id = None, extra_pnginfo=None):
         if unique_id and extra_pnginfo and "workflow" in extra_pnginfo[0]:
             workflow = extra_pnginfo[0]["workflow"]
             node = next((x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]), None)
             if node:
-                node["widgets_values"] = [text]        
+                node["widgets_values"] = [text]
         return {"ui": {"text": text}, "result": (text,)}
-
 
 NODE_CLASS_MAPPINGS = {
     "ShowText": ShowText,
