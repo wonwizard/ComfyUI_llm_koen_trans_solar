3aa5a8a123a9266b9c9b16f8d06038f37597cfe5
wonwizard <54572615+wonwizard@users.noreply.github.com>
2024-03-05 21:57:09 +0900

    Update node_LLM_CLIPTextEncode.py

--- node_LLM_CLIPTextEncode.py
+++ node_LLM_CLIPTextEncode.py
@@ -2,7 +2,7 @@ class LLM_CLIPTextEncode:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": 
-                {"itext": ("STRING", {"multiline": True, "default": " "}), 
+                {"text": ("STRING", {"multiline": True, "default": " "}), 
                  "intext": ("STRING", {"forceInput": True}),    
                  "clip": ("CLIP", )}
                  }
@@ -11,12 +11,12 @@ class LLM_CLIPTextEncode:
 
     CATEGORY = "LLM Translate"
 
-    def encode(self, intext, clip, itext):
-        itext = intext + itext
+    def encode(self, intext, clip, text):
+        text = intext + text
         tokens = clip.tokenize(text)
         cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
         #return ([[cond, {"pooled_output": pooled}]], )
-        return {"ui": {"itext": itext}, "result": ([[cond, {"pooled_output": pooled}]], )}
+        return {"ui": {"text": "test"}, "result": ([[cond, {"pooled_output": pooled}]], )}
 
 NODE_CLASS_MAPPINGS = {
     "LLM_CLIPTextEncode": LLM_CLIPTextEncode,
